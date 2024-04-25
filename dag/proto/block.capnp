c9558492141b78a0883c17a40cb8c9ee269d6008
insiderbyte <164867667+insiderbyte@users.noreply.github.com>
2024-04-25 18:47:57 +0300

    block_id (#32)

--- dag/proto/block.capnp
+++ dag/proto/block.capnp
@@ -10,3 +10,9 @@ struct Block {
     batches @2 :List(Data);
     parents @3 :List(Data);
 }
+
+struct BlockID {
+    round @0 :UInt64;
+    signer @1 :Data;
+    hash @2 :Data;
+}
--- dag/proto/block.capnp.go
+++ dag/proto/block.capnp.go
@@ -140,27 +140,131 @@ func (f Block_Future) Struct() (Block, error) {
 	return Block(p.Struct()), err
 }
 
-const schema_ebe99359e631e3a9 = "x\xda\\\xc8\xb1J\xc3P\x14\x06\xe0\xff?7\xb5t" +
-	"H\xe9\x85\xe0\xea\x03\x88\x96:vQ\xba\x0b=\xa3 " +
-	"\xe2m\x12\xda\xd2\x90\x1b\xd2\x08.R\x17qW_\xc2" +
-	"\xa7\xc8\xe4\xe8\xe0\xe6&((\xe2CD\xe2\xe8\xf4\xc1" +
-	"7\xb8=\x0aFaM\x88F\x9d\xad\xe6\xec\xb8\xbey" +
-	"\xdd}\xbb\x83n\x93\xcd\xe3\xc7\xe8\xf3\xe4\xfe\xfb\x07\x1d" +
-	"\xd3\x05\xec\xd3\xbb}i}\xfe\xc2^\x93\xb8\xf9\xb0(" +
-	"}%~8\xcb|\xbc\xda\x8f]\x91\x17\xe3I\xd6\xf5" +
-	"\xf1jJ\xea\xc0\x04@@\xc0\xba\x03@O\x0du!" +
-	"$#\xb6\x97\x8e\x01=7\xd4Lh\x85\x11\x05\xb0\xcb" +
-	"\x09\xa0\x89\xa1^\x0b\xad\x91\x88\x06\xb0Wm^\x1a\xea" +
-	"\x83p\xa7\xf4\x17y\xc2\x1e\x84=\xf0p\xbd\x9c\xe7i" +
-	"\xc9\x10\xc2\x10\xdc\xcc\\\x15/\xd25\xfb\xe0\xd4\xf0\xaf" +
-	"\xfb\xe0\xa6pe\x9aW\xff\xfb7\x00\x00\xff\xff$x" +
-	"5\xa6"
+type BlockID capnp.Struct
+
+// BlockID_TypeID is the unique identifier for the type BlockID.
+const BlockID_TypeID = 0xe8be3e5a06a33e53
+
+func NewBlockID(s *capnp.Segment) (BlockID, error) {
+	st, err := capnp.NewStruct(s, capnp.ObjectSize{DataSize: 8, PointerCount: 2})
+	return BlockID(st), err
+}
+
+func NewRootBlockID(s *capnp.Segment) (BlockID, error) {
+	st, err := capnp.NewRootStruct(s, capnp.ObjectSize{DataSize: 8, PointerCount: 2})
+	return BlockID(st), err
+}
+
+func ReadRootBlockID(msg *capnp.Message) (BlockID, error) {
+	root, err := msg.Root()
+	return BlockID(root.Struct()), err
+}
+
+func (s BlockID) String() string {
+	str, _ := text.Marshal(0xe8be3e5a06a33e53, capnp.Struct(s))
+	return str
+}
+
+func (s BlockID) EncodeAsPtr(seg *capnp.Segment) capnp.Ptr {
+	return capnp.Struct(s).EncodeAsPtr(seg)
+}
+
+func (BlockID) DecodeFromPtr(p capnp.Ptr) BlockID {
+	return BlockID(capnp.Struct{}.DecodeFromPtr(p))
+}
+
+func (s BlockID) ToPtr() capnp.Ptr {
+	return capnp.Struct(s).ToPtr()
+}
+func (s BlockID) IsValid() bool {
+	return capnp.Struct(s).IsValid()
+}
+
+func (s BlockID) Message() *capnp.Message {
+	return capnp.Struct(s).Message()
+}
+
+func (s BlockID) Segment() *capnp.Segment {
+	return capnp.Struct(s).Segment()
+}
+func (s BlockID) Round() uint64 {
+	return capnp.Struct(s).Uint64(0)
+}
+
+func (s BlockID) SetRound(v uint64) {
+	capnp.Struct(s).SetUint64(0, v)
+}
+
+func (s BlockID) Signer() ([]byte, error) {
+	p, err := capnp.Struct(s).Ptr(0)
+	return []byte(p.Data()), err
+}
+
+func (s BlockID) HasSigner() bool {
+	return capnp.Struct(s).HasPtr(0)
+}
+
+func (s BlockID) SetSigner(v []byte) error {
+	return capnp.Struct(s).SetData(0, v)
+}
+
+func (s BlockID) Hash() ([]byte, error) {
+	p, err := capnp.Struct(s).Ptr(1)
+	return []byte(p.Data()), err
+}
+
+func (s BlockID) HasHash() bool {
+	return capnp.Struct(s).HasPtr(1)
+}
+
+func (s BlockID) SetHash(v []byte) error {
+	return capnp.Struct(s).SetData(1, v)
+}
+
+// BlockID_List is a list of BlockID.
+type BlockID_List = capnp.StructList[BlockID]
+
+// NewBlockID creates a new list of BlockID.
+func NewBlockID_List(s *capnp.Segment, sz int32) (BlockID_List, error) {
+	l, err := capnp.NewCompositeList(s, capnp.ObjectSize{DataSize: 8, PointerCount: 2}, sz)
+	return capnp.StructList[BlockID](l), err
+}
+
+// BlockID_Future is a wrapper for a BlockID promised by a client call.
+type BlockID_Future struct{ *capnp.Future }
+
+func (f BlockID_Future) Struct() (BlockID, error) {
+	p, err := f.Future.Ptr()
+	return BlockID(p.Struct()), err
+}
+
+const schema_ebe99359e631e3a9 = "x\xda\x84\xcf\xcdJ:Q\x18\xc7\xf1\xdf\xef\x1c_\x10" +
+	"\xfe\xfawP\xaa\x9d\xfb\xa2\xc4\x96.L\xc4 !\xc1" +
+	"Cm\x8a\x88\xc6qpD\x99\x19F\x836a\x9bn" +
+	"\xa0\xba\x83V\xad\xba\x83Zu\x0f\xed\x82\x82^\x88\x16" +
+	"\x05-\\\xc4\xc4\x09zA\x88V\xe7<_\x9e\xc5\xf3" +
+	"I_\x94#\x85\xe4\xa4\x80PS\xd1X\xb8Y?\xdf" +
+	"\xbf\x9c\xb9:\x80\x9a \xc3\x93\x9b\xc2\xed\xda\xe1\xc3#" +
+	"\xa22\x0e\x18\xcf\xd7\xc6\x9b~Gw`\xb8R:\x8e" +
+	"\xad\x97\xce\xee\xc77E\x1c\xc8<\xf1%3\xa2\xfe\xbd" +
+	"\xf2\x14\xb3a\xcbl\xe7\xfd\xc0\x1b\x08/\xdf\xecyV" +
+	"w\xce2}\xd7/Vzq\xcf\xea6H\x95\x96\x11" +
+	" B\xc00\xe7\x01\xb5!\xa9\x1cA2K\xdd\xec\"" +
+	"\xa0\xb6$UO\xd0\x10\xccR\x00F\xa7\x02\xa8\x96\xa4" +
+	"\xda\x134\xa4\xc8R\x02\xc6\xae\x8e;\x92\xeaH0\x17" +
+	"x\xdbn\x8b\x09\x08&\xc0\x85~\xa7\xed\xda\x01\x93\x10" +
+	"L\x82\xc3\xa69\xb0\x1c\xbb\xcf\x14\xd8\x90\xfc\xc8)p" +
+	"\xe8\x9b\x81\xed\x0e\xc6\xf3\xef\x80\x9cgukUM\xf8" +
+	"\xf7EX\xd4\x84\xb2\xa4Z\xfe&\xd44\xa1*\xa9\x1a" +
+	"?\x08\xf5i@-I\xaa\xd5?\xae\xfd\xef\x98}\xe7" +
+	"sx\x0f\x00\x00\xff\xff&V_\x1f"
 
 func RegisterSchema(reg *schemas.Registry) {
 	reg.Register(&schemas.Schema{
 		String: schema_ebe99359e631e3a9,
 		Nodes: []uint64{
 			0x92df2bd885bf4d5e,
+			0xe8be3e5a06a33e53,
 		},
 		Compressed: true,
 	})
